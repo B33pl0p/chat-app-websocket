a039576f2fac858dc5fb52995350a96dcf1a6d8b
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2024-12-11 19:16:07 +0545

    utils `validate.ts` function to validate page dynamically

---
+++ src/utils/validate.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+// Helper function for validating email and password
+export const validateLoginRegisterSchema = (isLogin: boolean) => {
+  return z.object({
+    email: z.string().email('Invalid email format'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    ...(isLogin ? {} : { confirmPassword: z.string().min(6, 'Confirm password is required') })
+  });
+};
