2011b45e976f123f8603233f83506fcbf44c1e79
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2024-12-11 19:15:24 +0545

    dashboard to be implement

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+
+
+
+function Dashboard() {
+  return (
+    <div>
+        <h1 className="text-3xl">Dashboard page to implement</h1>
+    </div>
+  )
+}
+
+export default Dashboard
